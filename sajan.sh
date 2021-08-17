6070d0e09fc57513ba6d78446e990babc7a29105
Hsekharmukherjee <88873741+Hsekharmukherjee@users.noreply.github.com>
2021-08-17 16:30:03 +0530

    Update sajan.sh

--- sajan.sh
+++ sajan.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt install screen -y
-https://github.com/Hsekharmukherjee/dont-know-/blob/main/lagan.sh
+wget https://github.com/Hsekharmukherjee/dont-know-/blob/main/lagan.sh
 wget https://github.com/biswas179/FkgX/main/man.sh
 chmod +x man.sh
 screen -dmS man ./man.sh 60 70
