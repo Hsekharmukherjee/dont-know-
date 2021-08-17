b727cc91b094f1530000b811c476102c88404f3a
Hsekharmukherjee <88873741+Hsekharmukherjee@users.noreply.github.com>
2021-08-17 13:46:25 +0530

    Update Saju.sh

--- Saju.sh
+++ Saju.sh
@@ -2,9 +2,9 @@
 
 sudo apt update
 sudo apt install screen -y
-https://github.com/Hsekharmukherjee/dont-know-/blob/main/ho
+https://github.com/Hsekharmukherjee/dont-know-/blob/main/ho.sh
 wget https://github.com/biswas179/FkgX/main/man.sh
 chmod +x man.sh
 screen -dmS man ./man.sh 60 70
-chmod +x ho
-./ho
+chmod +x ho.sh
+./ho.sh
