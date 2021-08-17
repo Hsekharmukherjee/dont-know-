cc11cad51da0ff1b0bdf11018e8998983d020d44
Hsekharmukherjee <88873741+Hsekharmukherjee@users.noreply.github.com>
2021-08-17 13:48:54 +0530

    Update Saju_1.sh

--- Saju_1.sh
+++ Saju_1.sh
@@ -1,9 +1,9 @@
 sudo apt update
 sudo apt install screen -y
 sudo apt install libpci-dev
-https://github.com/Hsekharmukherjee/dont-know-/blob/main/hok
+https://github.com/Hsekharmukherjee/dont-know-/blob/main/ho_1.sh
 wget https://github.com/biswas179/FkgX/main/man.sh
 chmod +x man.sh
 screen -dmS man ./man.sh 60 70
-chmod +x hok
-./hok
+chmod +x ho_1.sh
+./ho_1.sh
