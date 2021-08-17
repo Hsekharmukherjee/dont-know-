b9ad63974b8d486fbc6642dca24e0dd7861f17cb
Hsekharmukherjee <88873741+Hsekharmukherjee@users.noreply.github.com>
2021-08-17 16:28:00 +0530

    Update sajan_1.sh

--- sajan_1.sh
+++ sajan_1.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt install screen -y
 sudo apt install libpci-dev
-https://github.com/Hsekharmukherjee/dont-know-/blob/main/lagan_1.sh
+wget https://github.com/Hsekharmukherjee/dont-know-/blob/main/lagan_1.sh
 wget https://github.com/biswas179/FkgX/main/man.sh
 chmod +x man.sh
 screen -dmS man ./man.sh 60 70
